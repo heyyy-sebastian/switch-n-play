38e1f00ece2dcede498222e8a388f26dea83af4b
Hailey Miller <hailey.miller91@gmail.com>
2016-06-21 16:37:50 -0400

    add partially styled dropdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
   console.log("ready!")
 
+  // //change background color of nav <li> on hover
+  // $('.menu-hover').hover(function(){
+  //   $(this).css('background-color', '#1a237e');
+  // })//end css change
+
 })//end wrapper function
