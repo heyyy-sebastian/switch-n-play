3f54d21881e8e5c1ab47b912a4a5faaba4a106d2
Hailey Miller <hailey.miller91@gmail.com>
2016-07-11 15:51:39 -0400

    Add more comments so other people know what things are

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,6 @@
 $(document).ready(function(){
   console.log("ready!")
 
-  // //change background color of nav <li> on hover
-  // $('.menu-hover').hover(function(){
-  //   $(this).css('background-color', '#1a237e');
-  // })//end css change
+
 
 })//end wrapper function
