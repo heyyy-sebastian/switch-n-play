e8d3e8f776320368764d3785bd5d7a284c1b9c62
mermatriarchy <hailey.miller91@gmail.com>
2016-11-22 16:05:09 -0500

    add updates to media pg w internal scroll functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,13 +6,26 @@ $(document).ready(function(){
   $('.homepage-carousel').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    dots: true,
+    //autoplay: true,
+    //autoplaySpeed: 2000,
+    //dots: true,
     arrows: true
   })
-  
 
+  //Smooth scrolling for internal links on media pg
+//Credit: https://paulund.co.uk/smooth-scroll-to-internal-links-with-jquery
+$('a[href^="#"]').on('click',function (e) {
+      e.preventDefault();
+
+      var target = this.hash;
+      var $target = $(target);
+
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+      }, 900, 'swing', function () {
+          window.location.hash = target;
+      });
+  });
 
   $(document).on('click.card', '.card', function (e) {
       console.log("clicked");
